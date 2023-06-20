0a250a26b4c6b9b37a34b1e84ead88e9f305c684
Constant Dupuis <cdu@pulsar.be>
2023-06-20 15:35:05 +0200

    Initial commit

---
+++ First/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Collections.ObjectModel;
+using System.Windows.Media;
+
+namespace First.ViewModel
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        public ObservableCollection<string> availableCOMPorts { get; set; } = new ObservableCollection<string>();
+
+        public MainWindowViewModel()
+        {
+            GetAvailableCOMPorts();
+        }
+
+        public void GetAvailableCOMPorts()
+        {
+            availableCOMPorts.Clear();
+
+            string[] ports = SerialPort.GetPortNames();
+
+            foreach (var port in ports)
+            {
+                availableCOMPorts.Add(port);
+            }
+        }
+    }
+}
---
+++ First/ViewModel/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First.ViewModel
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
