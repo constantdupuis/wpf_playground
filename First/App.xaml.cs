9dbc41b0f64b52babdce51173b5e4d9d43432adc
Constant Dupuis <cdu@pulsar.be>
2023-06-22 17:29:35 +0200

    Start adding DI

--- First/App.xaml.cs
+++ First/App.xaml.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Windows;
+using System.Windows.Navigation;
 
 namespace First
 {
@@ -13,5 +10,32 @@ namespace First
     /// </summary>
     public partial class App : Application
     {
+        public static IHost? AppHost { get; private set; }
+
+        public App()
+        {
+            AppHost = Host.CreateDefaultBuilder()
+                .ConfigureServices((hostContext, serviceq) => {
+
+                })
+                .Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await AppHost!.StartAsync();
+
+            var startupForm = AppHost.Services.GetRequiredService<MainWindow>();
+
+            startupForm.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+
+            base.OnExit(e);
+        }
     }
 }
